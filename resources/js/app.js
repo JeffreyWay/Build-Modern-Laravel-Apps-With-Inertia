817624b067221b17820f005549a33f44420f0756
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-10-01 13:10:22 -0400

    Episode 5 Complete

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,6 @@
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { InertiaProgress } from '@inertiajs/progress';
 
 createInertiaApp({
   resolve: name => require(`./Pages/${name}`),
@@ -8,4 +9,9 @@ createInertiaApp({
       .use(plugin)
       .mount(el);
   },
+});
+
+InertiaProgress.init({
+  color: 'red',
+  showSpinner: true,
 });
