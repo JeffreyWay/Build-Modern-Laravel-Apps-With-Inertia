4cc92ecd0acbc9ec24809c838330c1272567703a
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-10-01 10:19:53 -0400

    Episode 3 Complete

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Home</h1>
+
+  <p>In this series, we will use the following frameworks:</p>
+
+  <ul>
+    <li v-for="framework of frameworks" v-text="framework"></li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    frameworks: Array,
+  },
+};
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home', [
+        'name' => 'Jeffrey Way',
+        'frameworks' => [
+            'Laravel', 'Vue', 'Inertia'
+        ]
+    ]);
 });
