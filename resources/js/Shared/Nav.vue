f9a9821c017258a91dc4c3c31729266ac44f906f
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-10-07 14:51:58 -0400

    Episode 8 Complete

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,20 +1,31 @@
 <template>
   <nav class="mt-6">
     <ul class="list-disc">
-      <li><Link href="/" class="text-blue-500 hover:underline">Home</Link></li>
-      <li><Link href="/users" class="text-blue-500 hover:underline">Users</Link></li>
-      <li><Link href="/settings" class="text-blue-500 hover:underline">Settings</Link></li>
       <li>
-        <Link href="/logout" method="post" as="button" class="text-blue-500 hover:underline">Log Out</Link>
+        <NavLink href="/" :active="$page.component === 'Home'">
+          Home
+        </NavLink>
+      </li>
+
+      <li>
+        <NavLink href="/users" :active="$page.component === 'Users'">
+          Users
+        </NavLink>
+      </li>
+
+      <li>
+        <NavLink href="/settings" :active="$page.component === 'Settings'">
+          Settings
+        </NavLink>
       </li>
     </ul>
   </nav>
 </template>
 
 <script>
-import { Link } from '@inertiajs/inertia-vue3';
+import NavLink from "./NavLink";
 
 export default {
-  components: { Link },
+  components: { NavLink },
 };
 </script>
---
+++ resources/js/Shared/NavLink.vue
@@ -0,0 +1,20 @@
+<template>
+  <Link
+    class="text-blue-500 hover:underline"
+    :class="{'font-bold underline': active }"
+  >
+    <slot />
+  </Link>
+</template>
+
+<script>
+import { Link } from "@inertiajs/inertia-vue3";
+
+export default {
+  components: { Link },
+
+  props: {
+    active: Boolean
+  }
+};
+</script>
