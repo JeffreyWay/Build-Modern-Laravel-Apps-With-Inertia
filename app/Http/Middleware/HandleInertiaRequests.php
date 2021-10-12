bc0baf0cfcb7de196ba5bfec9a1d726438be26f7
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-10-12 09:53:41 -0400

    Episode 10 Complete

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -37,7 +37,11 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
-            //
+            'auth' => [
+                'user' => [
+                    'username' => 'JohnDoe'
+                ]
+            ]
         ]);
     }
 }
--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,9 +1,15 @@
 <template>
   <section class="p-6 bg-gray-200">
     <header class="flex justify-between">
-      <h1 class="font-bold text-lg">
-        My App
-      </h1>
+      <div class="flex items-center">
+        <h1 class="font-bold text-lg">
+          My App
+        </h1>
+
+        <p class="text-sm ml-4">
+          Welcome Back, {{ username }}!
+        </p>
+      </div>
 
       <Nav />
     </header>
@@ -20,6 +26,12 @@
 import Nav from "./Nav";
 
 export default {
-  components: { Nav }
+  components: { Nav },
+
+  computed: {
+    username() {
+      return this.$page.props.auth.user.username;
+    }
+  }
 };
 </script>
