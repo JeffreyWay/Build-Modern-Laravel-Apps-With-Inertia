cc701d409822f39d6ed8395788a264ac281fe131
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-10-25 15:03:47 -0400

    Remove iframe

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -17,15 +17,6 @@
 
   <section class="p-6">
     <div class="max-w-3xl mx-auto">
-      <iframe
-        class="mb-6"
-        width="100%"
-        frameborder="no"
-        scrolling="no"
-        seamless
-        src="https://player.simplecast.com/fd0bd2ba-c553-466c-a060-b144797ce369?dark=false"
-      />
-
       <slot />
     </div>
   </section>
