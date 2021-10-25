e8461059d1db44d76d7adf3f6de18073b5331eea
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-10-25 15:08:16 -0400

    Compile

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,31 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:vue/vue3-recommended"
-  ],
-  "parserOptions": {
-    "ecmaVersion": 12,
-    "sourceType": "module"
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended"],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
   },
-  "plugins": [
-    "vue"
-  ],
-  "rules": {
-    "indent": [
-      "error",
-      2
-    ],
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "quotes": [
-      "error",
-      "double"
-    ],
-    "semi": [
-      "error",
-      "always"
-    ],
+  plugins: ["vue"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
 
     "vue/order-in-components": ["error"],
     "vue/require-default-prop": ["off"],
-    "vue/max-attributes-per-line": ["error", {
-      "singleline": {
-        "max": 2,
-        "allowFirstLine": true
-      }
-    }]
-  }
+    "vue/max-attributes-per-line": [
+      "error",
+      {
+        singleline: {
+          max: 2,
+          allowFirstLine: true,
+        },
+      },
+    ],
+  },
+  globals: { require: true },
 };
