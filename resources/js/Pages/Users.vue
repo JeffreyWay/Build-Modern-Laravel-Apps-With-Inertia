cdd7eeabd740755c43846e6ffeed1263b8cfbb05
Jeffrey Way <jeffrey@jeffrey-way.com>
2021-10-28 11:20:55 -0400

    Episode 17 Complete

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,19 +1,45 @@
 <template>
   <Head title="Users" />
 
-  <h1 class="text-3xl">
+  <h1 class="text-3xl mb-6">
     Users
   </h1>
 
-  <ul>
-    <li
-      v-for="user in users"
-      :key="user.id"
-      v-text="user.name"
-    />
-  </ul>
+  <div class="flex flex-col">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+        <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+          <table class="min-w-full divide-y divide-gray-200">
+            <tbody class="bg-white divide-y divide-gray-200">
+              <tr v-for="user in users.data" :key="user.id">
+                <td class="px-6 py-4 whitespace-nowrap">
+                  <div class="flex items-center">
+                    <div>
+                      <div class="text-sm font-medium text-gray-900">
+                        {{ user.name }}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+
+                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                  <Link :href="`/users/${user.id}/edit`" class="text-indigo-600 hover:text-indigo-900">
+                    Edit
+                  </Link>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <Pagination :links="users.links" class="mt-6" />
 </template>
 
 <script setup>
-defineProps({ users: Array });
+import Pagination from "../Shared/Pagination";
+
+defineProps({ users: Object });
 </script>
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,8 @@ Route::get('/', function () {
 
 Route::get('/users', function () {
     return Inertia::render('Users', [
-        'users' => User::all()->map(fn($user) => [
+        'users' => User::paginate(10)->through(fn($user) => [
+            'id' => $user->id,
             'name' => $user->name
         ])
     ]);
